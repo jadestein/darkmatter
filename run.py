0532ef8ff0b702f25571ece40525403213352927
Adam Scheck <adambryce12@gmail.com>
2019-04-19 08:01:07 -0400

    הדסלןחהד

--- run.py
+++ run.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/<men>')
+def index(men):
+    return render_template('index.html', var=men)
 
 app.run(debug=True, host='0.0.0.0')

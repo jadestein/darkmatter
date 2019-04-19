adec5430c27834d5a6c90ca575193092affbbb21
Henry LeCompte <lecompteh18@gmail.com>
2019-04-19 08:58:28 -0400

    changed spacing

--- run.py
+++ run.py
@@ -2,8 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/<men>')
 def index(men):
     return render_template('index.html', var=men)
+
 
 app.run(debug=True, host='0.0.0.0')

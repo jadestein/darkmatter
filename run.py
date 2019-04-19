e0375dd5edf2ba472664fa1a2437f3349c5b0368
Adam Scheck <adambryce12@gmail.com>
2019-04-19 07:55:42 -0400

    adam the best one

--- run.py
+++ run.py
@@ -1,21 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return('Baby Girl')
-
-@app.route('/adam')
-def adam():
-    return('Call me at (305) 965-9445')
-
-@app.route('/steven')
-def steven():
-    return('I love Adam because he is the best son ever. Adam will go to MIT.')
-
-@app.route('/<number>')
-def hello(number):
-    return('Hello. That is a great lucky number. My favorite is also #' + number)
+    return render_template('index.html')
 
 app.run(debug=True, host='0.0.0.0')

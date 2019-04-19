75570c7baffab38f60dca0954002546983b1a155
Adam Scheck <adambryce12@gmail.com>
2019-04-19 00:07:49 -0400

    adam do great jop in python nobody delete or i smack you with my horse good night

--- run.py
+++ run.py
@@ -6,4 +6,16 @@ app = Flask(__name__)
 def index():
     return('Baby Girl')
 
-app.run(debug=True)
+@app.route('/adam')
+def adam():
+    return('Call me at (305) 965-9445')
+
+@app.route('/steven')
+def steven():
+    return('I love Adam because he is the best son ever. Adam will go to MIT.')
+
+@app.route('/<number>')
+def hello(number):
+    return('Shalom. That is a great lucky number. My favorite is also #' + number)
+
+app.run(debug=True, host='0.0.0.0')
